c737356493d1313beb526f00520f1b3b9bdc2684
Lucas Pinheiro <49772689+LucasPinheiiro@users.noreply.github.com>
2024-07-03 13:16:37 +0000

    get item added

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World?')
+  res.send('Hello World')
 })
 app.get('/oi', function (req, res) {
     res.send('Olá, mundo!?')
   })
+
+//lista de personangens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+//READ all - [GET] /item
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
 
 app.listen(3000)
